4a4839f681c406a1dae702fae61bcc65bb13baa8
afifmaulana <afifmaulanaiskandar@gmail.com>
2023-07-08 09:33:39 +0700

    Portfolio

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -53,3 +53,19 @@ function scrollToTop() {
         event.preventDefault();
         $(this).ekkoLightbox();
     });
+
+  /**
+   * Hero type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
